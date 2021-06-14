1494db3b352e64febb708aa08eed5723d445abe3
mireie <michael.reiersgaard@gmail.com>
2021-06-14 16:41:24 -0700

    WIP: add js
    Co-authored-by: Godfrey Owidi <godfreyowidi@users.noreply.github.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,20 @@
 
 function FavoritePlaces() {
   this.places = {};
-  this.currentId=0;
+  this.currentId = 0;
 }
 
-FavoritePlaces.prototype.addPlace = function(place) {
+FavoritePlaces.prototype.addPlace = function (place) {
   place.id = this.assignId();
   this.places[place.id] = place;
 };
 
-FavoritePlaces.prototype.assignId = function() {
+FavoritePlaces.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 };
 
-FavoritePlaces.prototype.findPlace = function(id) {
+FavoritePlaces.prototype.findPlace = function (id) {
   if (this.places[id] != undefined) {
     return this.places[id];
   }
@@ -33,19 +33,37 @@ function Place(userName, userDate, userPlace) {
 // Interface Logic
 let favoritePlaces = new FavoritePlaces();
 
-$(document).ready(function() {
-  $("form#user-entry").submit(function(event) {
+function displayPlaceDetails(placeToDisplay) {
+  let placesList = $('ul#places');
+  let htmlForPlaceInfo = "";
+  Object.keys(placeToDisplay.places).forEach(function(key) {
+    const place = placeToDisplay.findPlace(key);
+    htmlForPlaceInfo += "<li id=" + place.id + ">" + place.placeName + "</li";
+  });
+  placesList.html(htmlForPlaceInfo);
+}
+
+function showPlace(placeId) {
+  const place = favoritePlaces.findPlace(placeId);
+  $('.user-name').html(place.userName);
+  $('.user-date').html(place.userDate);
+  $('.user-place').html(place.userPlace);
+}
+
+$(document).ready(function () {
+  $("form#user-entry").submit(function (event) {
     event.preventDefault();
     const userDate = $('#user-date').val();  //Example Input: 2021-06-01
     const userName = $('#user-name').val();
-    const userPlace = $('#user-place').val(); 
-    let newPlace = new Place(userDate,userName,userPlace);
+    const userPlace = $('#user-place').val();
+    let newPlace = new Place(userName, userDate, userPlace);
     favoritePlaces.addPlace(newPlace);
-    $(".output").toggle();
-    $(".entry").toggle();
+    $(".output").show();
+    $(".entry").hide();
   });
-  $(".output").click(function(event) {
-    $(".output").slideToggle();
-    $(".entry").slideToggle();
+  $(".output").click(function (event) {
+    $(".output").hide();
+    $(".entry").show();
   });
+
 });
